8319b9d8cd9003ee946488de53591ccb2e6cc46a
Riley Gowanlock <rgowanlock@gmail.com>
2017-03-05 18:47:46 -0800

    Make title page b&w on scroll

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
-$(document).foundation()
+$(document).ready(function() {
+    $(document).foundation();
+})
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if ($(window).scrollTop() > 50){
+            $("#titlePage").css({"filter": "contrast(220%) grayscale(100%) brightness(40%)",
+                                 "-webkit-filter": "contrast(220%) grayscale(100%) brightness(40%)"});
+            $("#job, #name").css({"filter": "grayscale(100%) brightness(40%)",
+                                  "-webkit-filter": "grayscale(100%) brightness(40%)"});
+        } else {
+            $("#titlePage").css({"filter": "contrast(220%) grayscale(0%) brightness(100%)",
+                                 "-webkit-filter": "contrast(220%) grayscale(0%) brightness(100%)"});
+            $("#job, #name").css({"filter": "grayscale(0%) brightness(100%)",
+                                 "-webkit-filter": "grayscale(0%) brightness(100%)"});
+        }
+    });
+});
