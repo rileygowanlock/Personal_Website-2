6956a318199c5b3e2ce079da3670366c2f903333
Riley Gowanlock <rgowanlock@gmail.com>
2017-05-06 14:00:57 -0400

    Finish about me page

--- js/app.js
+++ js/app.js
@@ -1,8 +1,5 @@
-$(document).ready(function() {
-    $(document).foundation();
-})
-
 $(document).ready(function(){
+    $(document).foundation();
     $(window).scroll(function(){
         if ($(window).scrollTop() > 50){
             $("#titlePage").css({"filter": "contrast(220%) grayscale(100%) brightness(40%)",
@@ -16,4 +13,36 @@ $(document).ready(function(){
                                  "-webkit-filter": "grayscale(0%) brightness(100%)"});
         }
     });
+    
+    var offset = $(".about-pics").offset().top;
+    $(window).scroll(function() {
+        if ($(window).scrollTop() >= offset) {
+            $('.about-pics').css({
+                position: 'fixed'
+            });
+            $('.main').css({
+                visibility: 'hidden'
+            });
+        } else {
+            $('.about-pics').css({
+                position: 'relative'
+            });
+            $('.main').css({
+                visibility: 'visible'
+            });
+        }
+    });
+    
+    var offset2 = $(".portfolio").offset().top;
+    $(window).scroll(function() {
+        if ($(window).scrollTop()>= offset2) {
+            $('.about-pics').css({
+                position: 'relative'
+            });
+        }
+    });
+});
+
+$(window).on('beforeunload', function(){
+  $(window).scrollTop(0);
 });
