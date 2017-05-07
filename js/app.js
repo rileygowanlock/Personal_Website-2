8ee2e01552b6675df7849e8f07606509dcbf10cc
Riley Gowanlock <rgowanlock@gmail.com>
2017-05-07 02:31:44 -0400

    Scrolling between pages
    Make fixed div scroll
    Add outline of Portfolio Page

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+var count = true;
 $(document).ready(function(){
     $(document).foundation();
     $(window).scroll(function(){
@@ -14,29 +15,94 @@ $(document).ready(function(){
         }
     });
     
-    var offset = $(".about-pics").offset().top;
+    var offset3 = $(".nextPage").offset().top;
     $(window).scroll(function() {
-        if ($(window).scrollTop() >= offset) {
-            $('.about-pics').css({
-                position: 'fixed'
-            });
+        if ($(window).scrollTop() >= offset3) {
             $('.main').css({
                 visibility: 'hidden'
             });
         } else {
-            $('.about-pics').css({
-                position: 'relative'
-            });
             $('.main').css({
                 visibility: 'visible'
             });
         }
     });
     
-    var offset2 = $(".portfolio").offset().top;
+//    var offset = $(".about-pics").offset().top;
+//    $(window).scroll(function() {
+//        if ($(window).scrollTop() >= offset && !count) {
+//            $('.about-pics').css({
+//                position: 'fixed',
+//                top: '0px'
+//            });
+//        } else {
+//            $('.about-pics').css({
+//                position: 'relative'
+//                
+//            });
+//        }
+//    });
+//    
+    var offset = $(".about-pics").offset().top;
+    var offset2 = $(".about-paragraph").offset().top + document.getElementById('paragraph').clientHeight;
     $(window).scroll(function() {
-        if ($(window).scrollTop()>= offset2) {
-            $('.about-pics').css({
+        var position;
+        var top;
+        if ($(window).scrollTop() < offset) {
+            count = 0;
+            position = 'relative';
+            top = 0;
+        } else if ($(window).scrollTop() + document.getElementById('about-fixed').clientHeight >= offset2) {
+            if (!count) {
+                count = 1;
+                top = $(window).scrollTop() - document.getElementById('about-fixed').clientHeight;
+                position = 'relative';
+            }
+        } else {
+            count = 0;
+            position = 'fixed';
+            top = 0;
+        }
+        $('.about-pics').css({
+            position: position,
+            top: top
+        });
+    });
+
+     
+//    var offset2 = $(".about-paragraph").offset().top + document.getElementById('paragraph').clientHeight;
+//    var top = document.getElementById('paragraph').clientHeight - document.getElementById('about-fixed').clientHeight;
+//    $(window).scroll(function() {
+//        if ($(window).scrollTop() + screen.height - 135 >= offset2) {
+//            $('.about-pics').css({
+//                position: 'relative',
+//                top: top
+//            });
+//        }
+//    });
+    
+//    var offset2 = $(".about-paragraph").offset().top + document.getElementById('paragraph').clientHeight;
+//    $(window).scroll(function() {
+//        if ($(window).scrollTop() + document.getElementById('about-fixed').clientHeight >= offset2 && !count) {
+//            var top = $(window).scrollTop() - document.getElementById('about-fixed').clientHeight;
+//            count++;
+//            $('.about-pics').css({
+//                position: 'relative',
+//                top: top
+//            });
+//        }
+//    });
+    
+    var offset4 = $(".project-desc").offset().top;
+    $(window).scroll(function() {
+        if ($(window).scrollTop() >= offset4) {
+            $('.project-desc').css({
+                position: 'fixed',
+                top: '0px',
+                left: '0px'
+            });
+        } else {
+            $('.project-desc').css({
                 position: 'relative'
             });
         }
